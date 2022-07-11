a88a0496ed9c2cac3b9ad227ec988176f34db1a1
Geno <genocdiaz@gmail.com>
2022-07-11 12:26:36 -0700

    Implements rough prototype of board game state

--- client/pages/match/column.tsx
+++ client/pages/match/column.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-
-const DefaultRows = 10;
+import * as BoardInterfaces from './board_interfaces'
 
 interface Props {
+  columnNum: number,
+  columnState: BoardInterfaces.BoardSpace[]
+  player: 1 | 2,
+  dispatch: React.Dispatch<BoardInterfaces.Action>
 }
 
-const Column: React.FC<Props> = () => {
-  const [columnState, setColumnState] = useState<(string)[] | []>(Array.from({ length: DefaultRows }))
-  const [nextOpenSpace, setNextOpenSpace] = useState<number>(0);
-  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    console.log('clicked')
-    e.stopPropagation();
-    if (nextOpenSpace === 10) return;
+const handleColor = (value: 0 | 1 | 2) => {
+  switch(value) {
+    case 1:
+      return 'bg-red-500'
+    case 2:
+      return 'bg-yellow-300'
+    default:
+      return 'bg-gray-300'
+  }
+}
 
-    let newColumnState = [...columnState];
-    newColumnState[nextOpenSpace] = 'red'
-    setColumnState(newColumnState)
-    setNextOpenSpace(nextOpenSpace + 1)
+const Column: React.FC<Props> = ({ columnNum, columnState, player, dispatch }) => {
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+
+    const nextOpenSpace = columnState.map(val => val.value).indexOf(0)
+    // Prevent turn
+    if (nextOpenSpace === -1) return
+
+    dispatch({ payload: {
+      columnNum,
+      player,
+      nextOpenSpace,
+    }})
   };
+
   return (
     <div className='flex flex-col-reverse'>
       {
-        columnState.map((val, index) => {
+        columnState.map(({ id, value }) => {
           return (
             <div
-              key={index}
+              key={id}
               onClick={(e: React.MouseEvent<HTMLElement>) => {
                 handleClick(e)
               }}
-              className={`m-[2px] w-[50px] h-[50px] rounded-full ${columnState[index] === 'red' ? 'bg-red-500' : 'bg-gray-300'}`}
+              className={`m-[2px] w-[50px] h-[50px] rounded-full ${handleColor(value)}`}
             />
           )
         })
--- client/pages/match/index.tsx
+++ client/pages/match/index.tsx
@@ -1,26 +1,54 @@
-import { useEffect, useState } from 'react'
+import { Reducer, useEffect, useReducer } from 'react'
 import type { NextPage } from 'next'
 import { io } from "socket.io-client";
 import Column from './column'
+import * as BoardInterfaces from './board_interfaces'
 
-const DEFAULT_NUM_COLUMNS = 12
+const DEFAULT_NUM_COLUMNS = 7
+const DEFAULT_NUM_ROWS = 7
+const INITIAL_BOARD_STATE: BoardInterfaces.State = {
+  player: 1,
+  boardState: Array.from({ length: DEFAULT_NUM_COLUMNS }, (val, index) => {
+    return {
+      columnId: index,
+      column: Array.from({ length: DEFAULT_NUM_ROWS }, (val, index) => {
+        return {
+          id: index,
+          value: 0,
+        }
+      })
+    }
+  })
+}
+
+const reducer:Reducer<BoardInterfaces.State, BoardInterfaces.Action> = (state: BoardInterfaces.State, action: BoardInterfaces.Action) => {
+  const newState = { ...state }
+  const { columnNum, player, nextOpenSpace } = action.payload
+  newState.boardState[columnNum].column[nextOpenSpace].value = player
+  newState.player = player === 1 ? 2 : 1
+  return newState
+}
 
 const Home: NextPage = () => {
+  const [board, dispatch] = useReducer(reducer, INITIAL_BOARD_STATE)
+  // TODO determine how to type the destructure of useReducer
+  const { boardState, player } = board 
   useEffect(() => {
     console.log('MOUNTING!')
     const socket = io('http://localhost:5000')
     console.log('Connection established via', socket)
   }, [])
+
   return (
     <div className="p-8">
       <h1 className="font-bold">ConnectMour Match Page</h1>
       <div className="flex">
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-        <Column />
+        {
+          boardState.map((val) => {
+            const { columnId, column } = val
+            return <Column key={columnId} columnNum={columnId} columnState={column} player={player} dispatch={dispatch} />
+          })
+        }
       </div>
     </div>
   )
